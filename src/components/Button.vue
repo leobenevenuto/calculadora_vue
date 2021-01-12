90fa832f91fe8272cf933bb61712485be0a2a523
Leonardo Benevenuto <leonardo.web02@gmail.com>
2021-01-12 06:46:05 -0300

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,54 @@
+<template>
+    <button @click="$emit('onClick', label)" class="button" :class="{double, triple, operation}" >
+        {{ label }}
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {},
+        operation: {type: Boolean },
+        double: {type: Boolean },
+        triple: {type: Boolean }
+    }
+}
+</script>
+
+<style>
+    :root{
+        --bg-button: #f0f0f0;
+        --border-button: solid 1px #888;
+    }
+
+    .button{
+        font-size: 1.4em;
+        background-color: var(--bg-button);
+        border: none;
+        border-right: var(--border-button);
+        border-bottom: var(--border-button);
+        outline: none;
+        cursor: pointer;
+    }
+
+    .button:active{
+        background-color: #CCC;
+    }
+
+    .button.double{
+        grid-column: span 2;
+    }
+
+    .button.triple{
+        grid-column: span 3;
+    }
+
+    .button.operation{
+        background-color: #fa8231;
+        color: #fff;
+    }
+
+    .button.operation:active{
+        background-color: #fa8231cc
+    }
+</style>
